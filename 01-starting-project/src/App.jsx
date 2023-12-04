0a69f51e337335a45cced39898b82996734e3295
Priyag Raj Sharma <priyagrajsharma2699@gmail.com>
2023-12-04 14:31:20 +0530

    outputting investment  results in a table in Result components.

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
 function handleChange(inputIdentifier, newValue) {
   setUserInput(prevUserInput => ({
       ...prevUserInput,
-      [inputIdentifier]: newValue
+      [inputIdentifier]: +newValue
   }));
 }
 
